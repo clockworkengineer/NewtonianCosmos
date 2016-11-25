fd50a7075635142c82bd00cedb615b0cd075b1de
clockworkengineer <robert_tizzard@hotmail.com>
2016-11-25 19:10:12 +0000

    Addition of help menu.

--- FPE_commandLineOptions.js
+++ FPE_commandLineOptions.js
@@ -28,16 +28,36 @@
 const commandLineArgs = require('command-line-args');
 
 const optionDefinitions = [
-    {name: 'taskfile', alias: 't', type: String, defaultValue: 'tasksToRunDetails.json'},
-    {name: 'watch', alias: 'w', type: String, defaultValue: 'watch'},
-    {name: 'destination', alias: 'd', type: String, defaultValue: 'desination'},
-    {name: 'name', alias: 'n', type: String, defaultValue: 'File Processing Engine'},
-    {name: 'logfile', alias: 'l', type: String}
+    {name: 'taskfile', alias: 't', type: String, defaultValue: 'tasksToRunDetails.json', Description: "Task File JSON file to run with."},
+    {name: 'watch', alias: 'w', type: String, defaultValue: 'watch', Description: "Default watch folder."},
+    {name: 'destination', alias: 'd', type: String, defaultValue: 'desination',  Description: "Default desination folder."},
+    {name: 'name', alias: 'n', type: String, defaultValue: 'File Processing Engine', Description: "Program desciption."},
+    {name: 'logfile', alias: 'l', type: String,  Description: "Log file name."},
+    {name: 'help', alias: 'h',  Description: "Help menu."}
 ];
+
+var options;
 
 try {
 
-    var options = commandLineArgs(optionDefinitions);
+   options = commandLineArgs(optionDefinitions);
+
+    // Display help menu and exit.
+    
+    if (options.help) {
+        console.log(options.name+"\n");
+        console.log("Command                      Desciption\n");
+        for (let option in optionDefinitions) {
+            let len = optionDefinitions[option].name.length+optionDefinitions[option].alias.length;
+            if (optionDefinitions[option].type) {
+                console.log("--%s,-%s arg%s %s ", optionDefinitions[option].name, optionDefinitions[option].alias," ".repeat(20-len), optionDefinitions[option].Description);
+            } else {
+                console.log("--%s,-%s %s %s", optionDefinitions[option].name, optionDefinitions[option].alias, " ".repeat(23-len), optionDefinitions[option].Description);
+
+            }
+        }
+        process.exit(1);
+    }
 
 } catch (e) {
     console.log(e.message);
